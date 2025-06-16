ee760c2755c6a173490511f4577f5d71dcf778f0
정유석 <elwlahstmxjf@naver.com>
2025-06-16 12:55:24 +0900

    feat: add annotation in ProdRedisConfig

--- src/main/java/bitnagil/bitnagil_backend/global/config/DevRedisConfig.java
+++ src/main/java/bitnagil/bitnagil_backend/global/config/DevRedisConfig.java
@@ -13,7 +13,6 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Profile({"dev", "local"})
-//@Profile({"local"})
 @Configuration
 @EnableRedisRepositories
 public class DevRedisConfig {
--- src/main/java/bitnagil/bitnagil_backend/global/config/ProdRedisConfig.java
+++ src/main/java/bitnagil/bitnagil_backend/global/config/ProdRedisConfig.java
@@ -7,6 +7,7 @@ import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Component;
@@ -15,6 +16,7 @@ import java.util.Arrays;
 
 @Component
 @Profile("prod")
+@EnableRedisRepositories
 public class ProdRedisConfig {
 
     @Value("${spring.data.redis.ssl.enabled}")
