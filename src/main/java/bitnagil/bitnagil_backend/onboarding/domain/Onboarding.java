da07851e6fdfad65374a4f4c9ba32f8904e24579
thisishwan2 <feel000617@gmail.com>
2025-07-12 13:47:34 +0900

    feat: entity 추가(Onboarding, Case, RecommendedRoutine, RecommendedSubRoutine)

---
+++ src/main/java/bitnagil/bitnagil_backend/onboarding/domain/Onboarding.java
@@ -0,0 +1,42 @@
+package bitnagil.bitnagil_backend.onboarding.domain;
+
+import bitnagil.bitnagil_backend.onboarding.domain.enums.*;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Onboarding {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long onboardingId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "varchar(40)") // mysql의 enum 타입을 사용하지 않도록 설정
+    @NotNull
+    private TimeSlot timeSlot;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "varchar(40)")
+    @NotNull
+    private EmotionType emotionType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "varchar(40)")
+    @NotNull
+    private RealOutingFrequency realOutingFrequency;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "varchar(40)")
+    @NotNull
+    private TargetOutingFrequency targetOutingFrequency;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "case_id", nullable = false)
+    private Case resultCase;
+}
