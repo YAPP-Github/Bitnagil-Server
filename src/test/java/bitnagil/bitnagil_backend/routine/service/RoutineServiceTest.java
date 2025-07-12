669bf665453bcdc6a68e30d45b0a73cb72b1efab
정유석 <elwlahstmxjf@naver.com>
2025-07-12 16:12:47 +0900

    refactor: 테스트 코드 수정

--- src/test/java/bitnagil/bitnagil_backend/routine/service/RoutineServiceTest.java
+++ src/test/java/bitnagil/bitnagil_backend/routine/service/RoutineServiceTest.java
@@ -45,33 +45,12 @@ class RoutineServiceTest {
 
         when(routineRequest.getRoutineName()).thenReturn("Morning Routine");
         when(routineRequest.getSubRoutineName()).thenReturn(List.of("손 씻기", "양치하기", "세수하기"));
-        when(routineRepository.existsByName("Morning Routine")).thenReturn(false);
 
         // when
         routineService.registerRoutine(user, routineRequest);
 
         // then
-        verify(routineRepository).existsByName("Morning Routine");
         verify(routineRepository).save(any(Routine.class));
         verify(subRoutineRepository, times(3)).save(any(SubRoutine.class));
-    }
-
-    @Test
-    @DisplayName("이미 해당 루틴이 등록되어 있는 경우 - 실패 케이스")
-    public void registerRoutine_DuplicateName_ThrowsException() {
-        // given
-        RoutineRequest routineRequest = mock(RoutineRequest.class);
-        when(routineRequest.getRoutineName()).thenReturn("Morning Routine");
-        when(routineRepository.existsByName("Morning Routine")).thenReturn(true);
-
-        // when & then
-        CustomException exception = assertThrows(CustomException.class, () -> {
-            routineService.registerRoutine(mock(User.class), routineRequest);
-        });
-
-        assertEquals(ErrorCode.ROUTINE_ALREADY_EXISTS, exception.getErrorCode());
-        verify(routineRepository).existsByName("Morning Routine");
-        verify(routineRepository, never()).save(any());
-        verify(subRoutineRepository, never()).save(any());
     }
 }
