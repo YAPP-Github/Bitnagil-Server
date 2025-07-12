1f0fa1fa62bc67f59ea9fb9411f9c979638b31bc
정유석 <elwlahstmxjf@naver.com>
2025-07-12 16:04:06 +0900

    refactor: END_DATE_TIME 수정

--- src/main/java/bitnagil/bitnagil_backend/global/utils/TimeUtils.java
+++ src/main/java/bitnagil/bitnagil_backend/global/utils/TimeUtils.java
@@ -4,5 +4,5 @@ import java.time.LocalDateTime;
 
 public class TimeUtils {
     public static final LocalDateTime END_DATE_TIME =
-        LocalDateTime.of(2099, 12, 31, 23, 59, 59);
+        LocalDateTime.of(9999, 12, 31, 23, 59, 59);
 }
