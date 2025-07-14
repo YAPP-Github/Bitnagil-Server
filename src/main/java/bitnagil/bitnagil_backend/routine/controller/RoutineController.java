dab465a70f020c6bd07348dd4dc544cdfb69ae40
정유석 <elwlahstmxjf@naver.com>
2025-07-14 18:12:00 +0900

    feat: 루틴 및 서브루틴 업데이트 API 추가

--- src/main/java/bitnagil/bitnagil_backend/routine/controller/RoutineController.java
+++ src/main/java/bitnagil/bitnagil_backend/routine/controller/RoutineController.java
@@ -1,5 +1,6 @@
 package bitnagil.bitnagil_backend.routine.controller;
 
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,21 +9,31 @@ import org.springframework.web.bind.annotation.RestController;
 import bitnagil.bitnagil_backend.global.annotation.CurrentUser;
 import bitnagil.bitnagil_backend.global.response.CustomResponseDto;
 import bitnagil.bitnagil_backend.routine.controller.spec.RoutineSpec;
-import bitnagil.bitnagil_backend.routine.request.RoutineRequest;
+import bitnagil.bitnagil_backend.routine.request.RegisterRoutineRequest;
+import bitnagil.bitnagil_backend.routine.request.UpdateRoutineRequest;
 import bitnagil.bitnagil_backend.routine.service.RoutineService;
 import bitnagil.bitnagil_backend.user.domain.User;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/api/v1/routine")
+@RequestMapping(value = "/api/v1/routines")
 public class RoutineController implements RoutineSpec {
 
     private final RoutineService routineService;
 
     @PostMapping("")
-    public CustomResponseDto<Object> createRoutine(@CurrentUser User user, @RequestBody RoutineRequest routineRequest) {
-        routineService.registerRoutine(user, routineRequest);
+    public CustomResponseDto<Object> registerRoutine(@CurrentUser User user,
+        @RequestBody RegisterRoutineRequest registerRoutineRequest) {
+        routineService.registerRoutine(user, registerRoutineRequest);
+
+        return CustomResponseDto.from(null);
+    }
+
+    @PatchMapping("")
+    public CustomResponseDto<Object> updateRoutine(@CurrentUser User user,
+        @RequestBody UpdateRoutineRequest updateRoutineRequest) {
+        routineService.updateRoutine(user, updateRoutineRequest);
 
         return CustomResponseDto.from(null);
     }
