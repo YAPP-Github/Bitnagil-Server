4b3601d62770789b447c3fa811d428b90132a531
정유석 <elwlahstmxjf@naver.com>
2025-07-16 17:22:22 +0900

    refactor: 인증 과정에서 유저 식별시 이력 시작 및 종료일시를 고려하여 현재 활성 사용자인지 검증 후, 유저 조회하도록 변경

--- src/main/java/bitnagil/bitnagil_backend/global/errorcode/ErrorCode.java
+++ src/main/java/bitnagil/bitnagil_backend/global/errorcode/ErrorCode.java
@@ -1,8 +1,5 @@
 package bitnagil.bitnagil_backend.global.errorcode;
 
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.UnsupportedJwtException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -35,7 +32,7 @@ public enum ErrorCode {
 
     // User 관련 에러 코드
     INACTIVE_USER("US000", HttpStatus.FORBIDDEN, "사용할 수 없는 사용자입니다."),
-    NOT_FOUND_USER("US001", HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
+    NOT_FOUND_USER("US001", HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다. (탈퇴 회원 가능성도 있습니다.)"),
 
     // User 인증 관련 에러 코드
     AGREEMENT_NOT_ACCEPTED("UA000", HttpStatus.BAD_REQUEST, "필수 약관에 모두 동의해야 서비스를 이용할 수 있습니다."),
--- src/main/java/bitnagil/bitnagil_backend/user/repository/UserRepository.java
+++ src/main/java/bitnagil/bitnagil_backend/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package bitnagil.bitnagil_backend.user.repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,7 +12,12 @@ import bitnagil.bitnagil_backend.user.domain.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, HistoryPk> {
+
     Optional<User> findBySocialTypeAndSocialId(SocialType socialType, String socialId);
+
+    // socialType, socialId 기반으로 유저 이력들 특정 후, 이력 시작일시 및 종료일시를 활용해서 현재시간 기준으로 유효한 유저 식별
+    Optional<User> findBySocialTypeAndSocialIdAndHistoryStartDateTimeLessThanEqualAndHistoryEndDateTimeGreaterThanEqual(
+        SocialType socialType, String socialId, LocalDateTime historyStartDateBound, LocalDateTime historyEndDateBound);
 
     Optional<User> findByUserPk(HistoryPk userPk);
 }
