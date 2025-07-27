f2b861f148afcf131faa1132e70501b3e4a2f157
정유석 <elwlahstmxjf@naver.com>
2025-07-27 17:02:37 +0900

    refactor: DTO 관련 로직을 RecommendedRoutineMapper로 책임을 위임
    1. searchRecommendedRoutines()의 로직 흐름을 한눈에 파악할 수 있도록 addPersonalizedRecommendedRoutine(), addCategoryRecommendedRoutines()로 분리하였습니다.
    
    2. recommendRoutinesByEmotionMarble()에서 비즈니스 로직에 의해 주로 List 초기화 및 for문 순회하는 부분이 반복되어 코드량이 비대해졌습니다. 스트림을 사용하여 짧게 가져가면 짧은 코드로도 어떤 행위를 하는지 파악할 수 있는 것이 장점으로 느껴져 변경하였습니다.

--- src/main/java/bitnagil/bitnagil_backend/recommendedRoutine/service/RecommendedRoutineMapper.java
+++ src/main/java/bitnagil/bitnagil_backend/recommendedRoutine/service/RecommendedRoutineMapper.java
@@ -1,10 +1,63 @@
 package bitnagil.bitnagil_backend.recommendedRoutine.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import bitnagil.bitnagil_backend.recommendedRoutine.domain.RecommendedRoutine;
+import bitnagil.bitnagil_backend.recommendedRoutine.domain.RecommendedSubRoutine;
+import bitnagil.bitnagil_backend.recommendedRoutine.response.RecommendedRoutineDto;
+import bitnagil.bitnagil_backend.recommendedRoutine.response.RecommendedRoutineSearchResult;
+import bitnagil.bitnagil_backend.recommendedRoutine.response.RecommendedSubRoutineDto;
+import bitnagil.bitnagil_backend.recommendedRoutine.response.RecommendedSubRoutineSearchResult;
 
 /**
  * 추천 루틴 관련해서 DB에서 조회해오거나 가공된 데이터를 DTO로 변환하는 Mapper 클래스입니다.
  */
 @Service
 public class RecommendedRoutineMapper {
+
+    // 추천 루틴을 DTO로 변환
+    public RecommendedRoutineDto toRecommendedRoutineDto(RecommendedRoutine recommendedRoutine,
+        List<RecommendedSubRoutineDto> recommendedRoutineDetailDtoList) {
+
+        return RecommendedRoutineDto.builder()
+            .recommendedRoutineId(recommendedRoutine.getRecommendedRoutineId())
+            .recommendedRoutineName(recommendedRoutine.getRecommendedRoutineName())
+            .routineDescription(recommendedRoutine.getRecommendedRoutineDescription())
+            .recommendedSubRoutines(recommendedRoutineDetailDtoList)
+            .build();
+    }
+
+    // 추천 서브 루틴을 DTO로 변환
+    public RecommendedSubRoutineDto toRecommendedSubRoutineDto(RecommendedSubRoutine recommendedSubRoutine) {
+
+        return RecommendedSubRoutineDto.builder()
+            .recommendedSubRoutineId(recommendedSubRoutine.getRecommendedSubRoutineId())
+            .recommendedSubRoutineName(recommendedSubRoutine.getSubRoutineName())
+            .build();
+    }
+
+    // 추천 루틴을 RecommendedRoutineSearchResult으로 변환
+    public RecommendedRoutineSearchResult toRecommendedRoutineSearchResult(
+        RecommendedRoutine recommendedRoutine, List<RecommendedSubRoutineSearchResult> recommendedSubRoutineResults) {
+
+        return RecommendedRoutineSearchResult.builder()
+            .recommendedRoutineId(recommendedRoutine.getRecommendedRoutineId())
+            .recommendedRoutineName(recommendedRoutine.getRecommendedRoutineName())
+            .recommendedRoutineDescription(recommendedRoutine.getRecommendedRoutineDescription())
+            .recommendedRoutineLevel(recommendedRoutine.getRecommendedRoutineLevel())
+            .recommendedSubRoutineDetailSearchResult(recommendedSubRoutineResults)
+            .build();
+    }
+
+    // 추천 서브 루틴을 RecommendedSubRoutineSearchResult으로 변환
+    public RecommendedSubRoutineSearchResult toRecommendedSubRoutineSearchResult(
+        RecommendedSubRoutine recommendedSubRoutine) {
+
+        return RecommendedSubRoutineSearchResult.builder()
+            .recommendedSubRoutineId(recommendedSubRoutine.getRecommendedSubRoutineId())
+            .recommendedSubRoutineName(recommendedSubRoutine.getSubRoutineName())
+            .build();
+    }
 }
--- src/main/java/bitnagil/bitnagil_backend/recommendedRoutine/service/RecommendedRoutineService.java
+++ src/main/java/bitnagil/bitnagil_backend/recommendedRoutine/service/RecommendedRoutineService.java
@@ -21,17 +21,16 @@ import bitnagil.bitnagil_backend.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -43,6 +42,8 @@ public class RecommendedRoutineService {
     private final EmotionMarbleRepository emotionMarbleRepository;
     private final UserRepository userRepository;
 
+    private final RecommendedRoutineMapper recommendedRoutineMapper;
+
     /**
      * 추천 카테고리별 루틴, 서브루틴을 조회
      * key: 추천 루틴 타입 value: 추천 루틴, 서브 루틴 리스트
@@ -52,45 +53,21 @@ public class RecommendedRoutineService {
 
         LocalDate nowDate = LocalDate.now();
 
-        // response 객체 생성
+        // 카테고리 별 추천루틴에 대한 response 객체 생성
         Map<RecommendedRoutineType, List<RecommendedRoutineSearchResult>> response = new HashMap<>();
         response.put(RecommendedRoutineType.PERSONALIZED, new ArrayList<>()); // 맞춤 루틴은 미리 초기화 한다.(감정구슬, 온보딩 결과를 넣기 위해)
 
         // 영속성 객체에 user를 저장하기 위해 user를 조회
+        
         user = userRepository.findByUserPk(user.getUserPk())
-                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
+            .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
 
-        /**
-         * 맞춤 추천(감정구슬 + 온보딩)을 조회한다.
-         */
-        // 감정구슬(당일에 감정구슬을 선택한 경우만 조회)
-        EmotionMarble emotionMarble = emotionMarbleRepository.findByUserIdAndDateIs(user.getUserPk().getId(), nowDate);
-        if(emotionMarble != null) { // 조회 결과가 존재하는 경우
-            makeEmotionMarbleResponse(emotionMarble, response);
-        }
+        // 맞춤 추천(감정구슬 + 온보딩)을 조회하고 response에 추가
+        EmotionMarble emotionMarble = addPersonalizedRecommendedRoutine(user, nowDate, response);
 
-        // 온보딩 결과에 따른 추천 루틴 조회
-        Onboarding onboarding = user.getOnboarding();
-        if (onboarding != null) { // 온보딩을 수행한 유저의 경우(온보딩은 필수지만 방어 로직으로 추가)
-            makeOnboardingResponse(onboarding, response);
-        }
+        // 맞춤추천 이외의 카테고리에 대한 추천 루틴을 response 추가
+        addCategoryRecommendedRoutines(response);
 
-        /**
-         * 맞춤추천이 아닌 나머지 추천 루틴 카테고리에 대해
-         */
-        RecommendedRoutineType[] values = RecommendedRoutineType.values();
-        for (RecommendedRoutineType value : values) {
-            // value가 PERSONALIZED가 아닌 경우에만 추천 루틴 조회
-            if (value == RecommendedRoutineType.PERSONALIZED) {
-                continue;
-            }
-            // 추천 루틴 조회(상위 4개만 조회)
-            List<RecommendedRoutine> recommendedRoutines = recommendedRoutineRepository.findTop4ByRecommendedRoutineTypeOrderByRecommendedRoutineIdAsc(value);
-            List<RecommendedRoutineSearchResult> recommendedRoutineResults = buildRecommendedRoutineSearchResult(
-                recommendedRoutines);
-            // Map에 값을 저장
-            response.put(value, recommendedRoutineResults);
-        }
         return RecommendedRoutineSearchResponse.builder()
                 .recommendedRoutines(response)
                 .emotionMarbleType(emotionMarble == null ? null : emotionMarble.getEmotionMarbleType()) // 감정 구슬 타입 설정
@@ -105,31 +82,55 @@ public class RecommendedRoutineService {
         }
 
         // 응답 생성
-        List<RecommendedRoutineDto> recommendedRoutineDtoList = new ArrayList<>();
-        for (RecommendedRoutine recommendedRoutine : recommendedRoutines) {
-            List<RecommendedSubRoutineDto> recommendedRoutineDetailDtoList = new ArrayList<>();
+        return recommendedRoutines.stream()
+            .map(this::toRecommendedRoutineDtoWithDetails)
+            .collect(Collectors.toList());
+    }
 
-            List<RecommendedSubRoutine> recommendedSubRoutines = recommendedSubRoutineRepository.findByRecommendedRoutine(recommendedRoutine);
+    private RecommendedRoutineDto toRecommendedRoutineDtoWithDetails(RecommendedRoutine recommendedRoutine) {
 
-            // 추천 루틴의 세부 루틴을 dto로 변환한다.
-            for (RecommendedSubRoutine recommendedSubRoutine : recommendedSubRoutines) {
-                RecommendedSubRoutineDto recommendedRoutineDetailDto = RecommendedSubRoutineDto.builder()
-                    .recommendedSubRoutineId(recommendedSubRoutine.getRecommendedSubRoutineId())
-                    .recommendedSubRoutineName(recommendedSubRoutine.getSubRoutineName())
-                    .build();
-                recommendedRoutineDetailDtoList.add(recommendedRoutineDetailDto);
+        List<RecommendedSubRoutineDto> recommendedRoutineDetailDtoList = recommendedSubRoutineRepository.findByRecommendedRoutine(
+                recommendedRoutine)
+            .stream()
+            .map(recommendedRoutineMapper::toRecommendedSubRoutineDto)
+            .toList();
+
+        // 추천 루틴을 dto로 변환한다.
+        return recommendedRoutineMapper.toRecommendedRoutineDto(recommendedRoutine, recommendedRoutineDetailDtoList);
+    }
+
+    private void addCategoryRecommendedRoutines(Map<RecommendedRoutineType, List<RecommendedRoutineSearchResult>> response) {
+        RecommendedRoutineType[] values = RecommendedRoutineType.values();
+
+        for (RecommendedRoutineType value : values) {
+            // value가 PERSONALIZED가 아닌 경우에만 추천 루틴 조회
+            if (value == RecommendedRoutineType.PERSONALIZED) {
+                continue;
             }
-
-            // 추천 루틴을 dto로 변환한다.
-            RecommendedRoutineDto recommendedRoutineDto = RecommendedRoutineDto.builder()
-                .recommendedRoutineId(recommendedRoutine.getRecommendedRoutineId())
-                .recommendedRoutineName(recommendedRoutine.getRecommendedRoutineName())
-                .routineDescription(recommendedRoutine.getRecommendedRoutineDescription())
-                .recommendedSubRoutines(recommendedRoutineDetailDtoList)
-                .build();
-            recommendedRoutineDtoList.add(recommendedRoutineDto);
+            // 추천 루틴 조회(상위 4개만 조회)
+            List<RecommendedRoutine> recommendedRoutines =
+                recommendedRoutineRepository.findTop4ByRecommendedRoutineTypeOrderByRecommendedRoutineIdAsc(value);
+            List<RecommendedRoutineSearchResult> recommendedRoutineResults = buildRecommendedRoutineSearchResult(
+                recommendedRoutines);
+            // Map에 값을 저장
+            response.put(value, recommendedRoutineResults);
         }
-        return recommendedRoutineDtoList;
+    }
+
+    private  EmotionMarble addPersonalizedRecommendedRoutine(User user, LocalDate nowDate,
+        Map<RecommendedRoutineType, List<RecommendedRoutineSearchResult>> response) {
+        // 감정구슬(당일에 감정구슬을 선택한 경우만 조회)
+        EmotionMarble emotionMarble = emotionMarbleRepository.findByUserIdAndDateIs(user.getUserPk().getId(), nowDate);
+        if(emotionMarble != null) { // 조회 결과가 존재하는 경우
+            makeEmotionMarbleResponse(emotionMarble, response);
+        }
+
+        // 온보딩 결과에 따른 추천 루틴 조회
+        Onboarding onboarding = user.getOnboarding();
+        if (onboarding != null) { // 온보딩을 수행한 유저의 경우(온보딩은 필수지만 방어 로직으로 추가)
+            makeOnboardingResponse(onboarding, response);
+        }
+        return emotionMarble;
     }
 
     private List<RecommendedRoutineSearchResult> buildRecommendedRoutineSearchResult(
@@ -151,24 +152,19 @@ public class RecommendedRoutineService {
     private void addRecommendedRoutineToResponse(RecommendedRoutine recommendedRoutine,
                                                  List<RecommendedSubRoutineSearchResult> recommendedSubRoutineResults,
                                                  List<RecommendedRoutineSearchResult> recommendedRoutineResults) {
-        RecommendedRoutineSearchResult recommendedRoutineResult  = RecommendedRoutineSearchResult.builder()
-                .recommendedRoutineId(recommendedRoutine.getRecommendedRoutineId())
-                .recommendedRoutineName(recommendedRoutine.getRecommendedRoutineName())
-                .recommendedRoutineDescription(recommendedRoutine.getRecommendedRoutineDescription())
-                .recommendedRoutineLevel(recommendedRoutine.getRecommendedRoutineLevel())
-                .recommendedSubRoutineDetailSearchResult(recommendedSubRoutineResults)
-                .build();
+
+        RecommendedRoutineSearchResult recommendedRoutineResult =
+            recommendedRoutineMapper.toRecommendedRoutineSearchResult(recommendedRoutine, recommendedSubRoutineResults);
         recommendedRoutineResults.add(recommendedRoutineResult);
     }
 
     // 추천 서브루틴을 응답 객체에 추가하는 메서드
     private void addRecommendedSubRoutineToResponse(List<RecommendedSubRoutine> recommendedSubRoutines,
                                                     List<RecommendedSubRoutineSearchResult> recommendedSubRoutineResults) {
+
         for (RecommendedSubRoutine recommendedSubRoutine : recommendedSubRoutines) {
-            RecommendedSubRoutineSearchResult recommendedSubRoutineResult = RecommendedSubRoutineSearchResult.builder()
-                    .recommendedSubRoutineId(recommendedSubRoutine.getRecommendedSubRoutineId())
-                    .recommendedSubRoutineName(recommendedSubRoutine.getSubRoutineName())
-                    .build();
+            RecommendedSubRoutineSearchResult recommendedSubRoutineResult =
+                recommendedRoutineMapper.toRecommendedSubRoutineSearchResult(recommendedSubRoutine);
             recommendedSubRoutineResults.add(recommendedSubRoutineResult);
         }
     }
