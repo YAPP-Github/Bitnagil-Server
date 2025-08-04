31acf3be75019ba6aa3d6115fd1971a82daa1808
thisishwan2 <feel000617@gmail.com>
2025-08-05 00:45:56 +0900

    feat: 컨트롤러 로깅 인터셉터 생성

---
+++ src/main/java/bitnagil/bitnagil_backend/global/config/WebConfig.java
@@ -0,0 +1,20 @@
+package bitnagil.bitnagil_backend.global.config;
+
+import bitnagil.bitnagil_backend.global.interceptor.LoggingInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoggingInterceptor loggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor)
+                .addPathPatterns("/**"); // 모든 경로
+    }
+}
