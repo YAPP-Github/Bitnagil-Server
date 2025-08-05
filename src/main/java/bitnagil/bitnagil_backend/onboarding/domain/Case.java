fc9865b263064f54a2e530770ac935431334316e
thisishwan2 <feel000617@gmail.com>
2025-08-06 01:20:38 +0900

    feat: @SQLDelete, @Where를 이용한 soft delete 추가

--- src/main/java/bitnagil/bitnagil_backend/onboarding/domain/Case.java
+++ src/main/java/bitnagil/bitnagil_backend/onboarding/domain/Case.java
@@ -6,11 +6,15 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "routine_case") // 이렇게 예약어 회피
+@SQLDelete(sql = "UPDATE routine_case SET deleted_at = NOW() WHERE case_id = ?")
+@Where(clause = "deleted_at IS NULL")
 public class Case extends BaseTimeEntity {
 
     @Id
@@ -18,7 +22,7 @@ public class Case extends BaseTimeEntity {
     private Long caseId;
 
     @Builder
-    public Case(Long caseId, String caseName) {
+    public Case(Long caseId) {
         this.caseId = caseId;
     }
 }
